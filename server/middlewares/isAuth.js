a666ef3fc68e70f8aeb907e2df728f862b356c31
Vansh Rekhi <vanshrekhi05@gmail.com>
2026-07-18 21:49:09 +0530

    Update isAuth.js

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -1,27 +1,26 @@
 import jwt from "jsonwebtoken"
 
 
-const isAuth = async (req,res,next) => {
+const isAuth = async (req, res, next) => {
     try {
-        let {token} = req.cookies
+        const authHeader = req.headers.authorization
 
-        if(!token){
-            return res.status(400).json({message:"user does not have a token"})
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(400).json({ message: "user does not have a token" })
         }
-        const verifyToken = jwt.verify(token , process.env.JWT_SECRET)
-        
-        if(!verifyToken){
-            return res.status(400).json({message:"user does not have a valid token"})
+
+        const token = authHeader.split(" ")[1]
+        const verifyToken = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (!verifyToken) {
+            return res.status(400).json({ message: "user does not have a valid token" })
         }
+
         req.userId = verifyToken.userId
-
         next()
-   
-
     } catch (error) {
-        return res.status(500).json({message:`isAuth error ${error}`})
+        return res.status(500).json({ message: `isAuth error ${error}` })
     }
-    
 }
 
 export default isAuth
